267241e5585dad4602927c57494696f0c4ddc896
assiazbr <zoubeirassia@gmail.com>
2020-11-15 16:02:34 +0100

    dernier commit

--- test2/src/test2/test2.java
+++ test2/src/test2/test2.java
@@ -3,6 +3,8 @@ package test2;
 public class test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("hello from assia");
+		System.out.print("hello from remote developperxxxxx");
+		int a =3;
+		int b =2;
 	}
 }
